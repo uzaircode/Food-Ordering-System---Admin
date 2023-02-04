2eec4c26e9eae35a81dc42fbef5c86666d291d8c
engkufaizz <sardinshelby@gmail.com>
2023-02-04 21:17:41 +0800

    faiz/userlogin&register
    dah add

---
+++ userlogin.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title></title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="split-screen">
+      <div class="left">
+        <section class="copy">
+          <h1>Take a Slice Out of Hunger</h1>
+          <p>All natural, fresh ingredients, housemade by chefs.</p>
+        </section>
+      </div>
+      <div class="right">
+        <form action="">
+          <section class="copy">
+            <h2>Sign In</h2>
+            <div class="login-container">
+              <p>Welcome back! Please enter your details.</p>
+            </div>
+          </section>
+          <div class="input-container email">
+            <label for="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email Address"
+            />
+          </div>
+          <div class="input-container password">
+            <label for="password">Password</label>
+            <input
+              id="password"
+              name="password"
+              placeholder="Must be at least 6 characters"
+              type="password"
+            />
+          </div>
+          <div class="input-container cta">
+            <label class="checkbox-container">
+              <input type="checkbox" />
+              <span class="checkmark"> </span>
+              Remember me
+            </label>
+          </div>
+          <button class="signup-btn" type="submit">Sign Up</button>
+          <section class="copy legal">
+            <p>
+              Don't have an account?
+              <strong> <a href="register.html">Sign up for free</a> </strong>
+            </p>
+          </section>
+        </form>
+      </div>
+    </div>
+
+    <script src="" async defer></script>
+  </body>
+</html>
