ce0131197cbd5862aa95eaa7d26fb4d06a3ebf63
Nik Uzair <nikuzairsc@gmail.com>
2023-02-08 02:10:09 +0800

    remove address email and phone form

--- payment.php
+++ payment.php
@@ -10,7 +10,7 @@ include('server.php');
 $customer_id = $_SESSION['customer_id'];
 
 
-// echo $customer_id;
+echo $customer_id;
 // echo $customer_email;
 
 
@@ -149,32 +149,8 @@ $customer_id = $_SESSION['customer_id'];
         <div class="col-75">
             <div class="container">
                 <form method="post" action="server.php">
+                    <input type="hidden" name="customer_id" value="<?php echo $customer_id; ?>">
                     <div class="row">
-                        <!-- <div class="col-50">
-                            <h3>Billing Address</h3>
-                            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-                            <input type="text" id="fname" name="firstname" placeholder="John M. Doe"
-                                value="<?php echo $_SESSION['customer_name']; ?>">
-                            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-                            <input type="text" id="email" name="email" placeholder="john@example.com"
-                                value="<?php echo $_SESSION['customer_email']; ?>">
-                            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-                            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-                            <label for="city"><i class="fa fa-institution"></i> City</label>
-                            <input type="text" id="city" name="city" placeholder="New York">
-
-                            <div class="row">
-                                <div class="col-50">
-                                    <label for="state">State</label>
-                                    <input type="text" id="state" name="state" placeholder="NY">
-                                </div>
-                                <div class="col-50">
-                                    <label for="zip">Zip</label>
-                                    <input type="text" id="zip" name="zip" placeholder="10001">
-                                </div>
-                            </div>
-                        </div> -->
-
                         <div class="col-50">
                             <h3>Payment</h3>
                             <label for="fname">Accepted Cards</label>
@@ -207,7 +183,6 @@ $customer_id = $_SESSION['customer_id'];
                                 </div>
                             </div>
                         </div>
-
                     </div>
                     <label>
                         <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
